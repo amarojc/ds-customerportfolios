eb92720de358cbd12362065e93db0e4ee0ece25e
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-03-20 23:28:26 -0300

    Project created

---
+++ backend/src/main/java/com/amarojc/customerportfolios/CustomerportfoliosApplication.java
@@ -0,0 +1,13 @@
+package com.amarojc.customerportfolios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerportfoliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerportfoliosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/amarojc/customerportfolios/CustomerportfoliosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarojc.customerportfolios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerportfoliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
