b300af8eb19265119e93cd3a6d8ee954ebbb42c7
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-03-24 18:42:16 -0300

    ClientRepository started

---
+++ backend/src/main/java/com/amarojc/customerportfolios/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.amarojc.customerportfolios.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.amarojc.customerportfolios.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
